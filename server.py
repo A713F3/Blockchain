5dab971187a0a8fd0ac6e183a241b958036b278e
Ali Efe <aliefekaragul@gmail.com>
2021-03-15 16:26:20 +0300

    added command lines

--- server.py
+++ server.py
@@ -15,17 +15,48 @@ class Server:
             self.clients = []
             self.adresses = {}
 
+    """
+        Start Server
+    """
+    def start(self):
+        self.s.listen()
+
+        print("Server is listening...")
+
+        # Start a thread for accepting clients
+        receive_thread = threading.Thread(target=self.receive)
+        receive_thread.start()  
+
+        # Start a thread for server commands
+        command_thread = threading.Thread(target=self.commands(), args=())
+        command_thread.start()
+    
+    """
+        Stop Server
+    """
+    def stop(self):
+        pickle.dump(self.adresses, open( "clients.p", "wb" ))
+        self.keep = False  
+        self.s.close()
+
         self.keep = True
 
+    """
+        Broadcast message to a client
+    """
     def broadcast(self, message, client):
         try:
             client.send(message)
         except:
             print("Broadcast Error!")
 
+        # For broadcasting a message to every client
         #for client in self.clients:
         #    client.send(message)
 
+    """
+        Handle each clients messages
+    """
     def handle(self, client, adress):
         while self.keep:
             try:
@@ -40,16 +71,15 @@ class Server:
                 client.close()
                 break
     
-    def commands(self):
-        command = input()
-
-        if command == "q":
-            self.stop()
-
+    """
+        Accept clients to server
+    """
     def receive(self):
         while self.keep:
+            # Accept client
             client, adress = self.s.accept()
             
+            # If client is known
             if client in self.clients: 
                 print(f"{self.adresses[adress]} is connected")
             else:
@@ -58,27 +88,19 @@ class Server:
                 self.clients.append(client)
                 self.adresses[adress] = "--"
             
-
+            # After aceepting client to start a thread for messages
             handle_thread = threading.Thread(target=self.handle, args=(client,adress))
             handle_thread.start()
-            
-            
-    def start(self):
-        self.s.listen()
 
-        print("Server is listening...")
+    """
+        For server commands
+    """      
+    def commands(self):
+        command = input()
 
-        receive_thread = threading.Thread(target=self.receive)
-        receive_thread.start()  
+        if command == "q":
+            self.stop()
 
-        command_thread = threading.Thread(target=self.commands(), args=())
-        command_thread.start()
-
-    def stop(self):
-        pickle.dump(self.adresses, open( "clients.p", "wb" ))
-        self.keep = False  
-        self.s.close()
-
-server = Server()
-
-server.start()
+if __name__ == "__main__":
+    server = Server()
+    server.start()
